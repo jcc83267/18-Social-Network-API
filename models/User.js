97e161067b15cbb03cf4387f8ce2b630aa74f9d8
James Canlas <jamescanlasjr@yahoo.com>
2021-04-17 16:35:22 -0700

    made user model

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            match: [/.+@.+ \ ..+/, 'Please enter a valid e-mail address'],
+            trim: true,
+
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false
+    }
+);
+
+// get friend count
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
